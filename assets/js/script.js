70891a197ca7f6b6033ef94838a8e74068525483
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2024-11-26 15:33:53 +0000

    JS element selection and eventlistener setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
 //Elements selection
 const form = document.querySelector('form');
-const emailInput = document.querySelector('email');
+const emailInput = document.querySelector('input[type = "text"]');
+
+
+//Thriggering when submit is clicked
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); //Prevent sending the email before verification
+
+    const emailValue = emailInput.value.trim();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    console.log(emailValue);
+})
